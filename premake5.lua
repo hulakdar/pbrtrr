44e5b5a7023c7e8d8153a98898f5a5458c2ebcf0
hulakdar <skamoza@gmail.com>
2020-03-15 22:50:54 +0200

    Init

---
+++ premake5.lua
@@ -0,0 +1,42 @@
+workspace "pbrtt"
+	architecture "x64"
+    startproject "pbrtrr"
+
+	configurations
+	{
+		"Debug",
+		"Development",
+		"Release"
+	}
+
+project "pbrtrr"
+    kind "WindowedApp"
+    language "C++"
+	cppdialect "C++17"
+    location "."
+    warnings "Extra"
+    objdir "./build/intermidiate"
+    targetdir ("./bin")
+
+    files { "./src/**.h", "./src/**.cpp" }
+    links { "d3dcompiler", "dxguid", "d3d12", "dxgi" }
+
+	filter "system:windows"
+        defines { "_CRT_SECURE_NO_WARNINGS", "WIN32", "_WINDOWS" }
+		systemversion "latest"
+
+    filter "configurations:Debug"
+        defines { "_DEBUG", "DEBUG" }
+        targetsuffix ("_Debug")
+		symbols "On"
+
+	filter "configurations:Development"
+		defines { "DEVELOPMENT" }
+        targetsuffix ("_Development")
+		symbols "On"
+		optimize "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG", "RELEASE" }
+        flags { "LinkTimeOptimization" }
+        optimize "On"
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+#include <winnt.h>
+#include <dxgi1_4.h>
+#include <iostream>
+#include <d3dcompiler.h>
+
+#include "external/d3dx12.h"
+#include "external/d3dx12Residency.h"
+
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow)
+{
+  return 0;
+}
