5453cd5af20217a62610e85c5e403ba8ccb6d605
hulakdar <skamoza@gmail.com>
2022-06-05 16:25:33 +0300

    forgor

---
+++ thirdparty/premake5.lua
@@ -0,0 +1,40 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "Off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+        "./imgui/imconfig.h",
+        "./imgui/imgui.h",
+        "./imgui/imgui.cpp",
+        "./imgui/imgui_draw.cpp",
+        "./imgui/imgui_internal.h",
+        "./imgui/imgui_tables.cpp",
+        "./imgui/imgui_widgets.cpp",
+        "./imgui/imstb_rectpack.h",
+        "./imgui/imstb_textedit.h",
+        "./imgui/imstb_truetype.h",
+         -- "./imgui/imgui_demo.cpp"
+        "./imnodes/imnodes.cpp"
+    }
+
+    includedirs {
+        "./imgui"
+    }
+
+    defines {
+        "IMGUI_DISABLE_OBSOLETE_FUNCTIONS",
+        "IMGUI_DISABLE_OBSOLETE_KEYIO"
+    }
+
+	filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "Off"
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
