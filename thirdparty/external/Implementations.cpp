77facbaaf59faba1804cb20c299c48a78ba781fd
hulakdar <skamoza@gmail.com>
2024-10-13 12:41:29 +0300

    missed file

---
+++ thirdparty/external/Implementations.cpp
@@ -0,0 +1,11 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "external/stb/stb_image.h"
+
+#define STB_SPRINTF_IMPLEMENTATION
+#include "external/stb/stb_sprintf.h"
+
+#define STB_DXT_IMPLEMENTATION
+#include "external/stb/stb_dxt.h"
+
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "external/stb/stb_image_resize.h"
